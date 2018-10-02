8a18134d420e10570774709dd62fb339cd5ecb9e
williamyeny <williamyeny@gmail.com>
2018-10-02 10:52:42 -0400

    added async requesting

--- scrape.py
+++ scrape.py
@@ -1,8 +1,10 @@
 import requests, json
 from bs4 import BeautifulSoup
+from requests_futures.sessions import FuturesSession
 
 totalProfiles = 100
 users = []
+session = FuturesSession()
 
 for i in range(totalProfiles/100):
   data = requests.get("https://www.roblox.com/search/users/results?keyword=fortnite&maxRows=100&startIndex=" + str(i*100)).json()
@@ -10,20 +12,23 @@ for i in range(totalProfiles/100):
 
   totalProfiles = data["TotalResults"]
 
+  profileRequests = {}
   for user in data["UserSearchResults"]:
     userUrl = user["UserProfilePageUrl"]
+    profileRequests[user["UserId"]] = session.get("https://roblox.com"+userUrl)
+    
 
-    soup = BeautifulSoup(requests.get("https://roblox.com"+userUrl).text, "html.parser")
-    date = soup.find("p", attrs={"class": "text-lead"}).text.split("P")[0]
-    print(date)
+  for user in data["UserSearchResults"]:
+    html = BeautifulSoup(profileRequests[user["UserId"]].result().text, "html.parser")
+    date = html.find("p", attrs={"class": "text-lead"}).text.split("P")[0]
 
     users.append({
       "name": user["Name"],
       "id": user["UserId"],
-      "url": userUrl,
       "date": date
     })
-    break
+
+    print("Profile " + str(user["UserId"]) + ": " + date)
 
 with open('data.json', 'w') as outfile:
     json.dump(users, outfile)
